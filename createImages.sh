4d45992482616f4a6028c035c20d5225c3b3ebf1
DeliciousRocks <waltersquires1@gmail.com>
2022-10-03 21:20:01 -0400

    Create createImages.sh

---
+++ createImages.sh
@@ -0,0 +1,5 @@
+jgraph -P Shivan_Dragon.jgr | ps2pdf - | convert -density 300 - -quality 100 Shivan_Dragon.jpg
+jgraph -P Black_Lotus.jgr | ps2pdf - | convert -density 300 - -quality 100 Black_Lotus.jpg
+jgraph -P Dark_Ritual.jgr | ps2pdf - | convert -density 300 - -quality 100 Dark_Ritual.jpg
+jgraph -P Giant_Growth.jgr | ps2pdf - | convert -density 300 - -quality 100 Giant_Growth.jpg
+jgraph -P Phyrexian_Compaetion.jgr | ps2pdf - | convert -density 300 - -quality 100 Phyrexian_Compaetion.jpg
