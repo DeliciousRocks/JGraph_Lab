7ed5f9d41a9a7dfc5ae323e4bae1194fd6bdce41
DeliciousRocks <waltersquires1@gmail.com>
2022-10-03 21:17:12 -0400

    Create printCards.sh

---
+++ printCards.sh
@@ -0,0 +1,5 @@
+python printCard.py Card_JSON/Shivan_Dragon.json Art_Assets/shivan_dragon.jpg
+python printCard.py Card_JSON/Black_Lotus.json Art_Assets/black_lotus.jpg
+python printCard.py Card_JSON/Dark_Ritual.json Art_Assets/dark_ritual.jpg
+python printCard.py Card_JSON/Giant_Growth.json Art_Assets/giant_growth.jpg
+python printCard.py Card_JSON/Phyrexian_Compleation.json Art_Assets/Phyrexian_Compleation.jpg
